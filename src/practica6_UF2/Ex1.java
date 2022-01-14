0c27c3561dbf0224b2a4d9f6ae55ed477b85625e
LINKAT <LINKAT@DESKTOP-LF6B84F>
2022-01-14 13:40:47 +0100

    Primer canvi (penjar treball)

---
+++ src/practica6_UF2/Ex1.java
@@ -0,0 +1,17 @@
+/**/
+package practica6_UF2;
+
+/**
+ *
+ * @author LINKAT
+ */
+public class Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
