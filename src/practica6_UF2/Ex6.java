e9929e90f56261c2a76962c460a404d944884e28
Ramon <Ramon@DESKTOP-LF6B84F>
2022-01-16 15:46:41 +0100

    1r canvi (penjar)

---
+++ src/practica6_UF2/Ex6.java
@@ -0,0 +1,11 @@
+/*6. Escriu un funció anomenada “Inicials” que rebi com a paràmetre una frase i
+torni un string amb les inicials de cada paraula convertides a majúscules*/
+package practica6_UF2;
+
+/**
+ *
+ * @author Ramon
+ */
+public class Ex6 {
+    
+}
