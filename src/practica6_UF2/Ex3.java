e64c2a5016517203fb7fcfd42ad2d67b1fb151ae
LINKAT <LINKAT@DESKTOP-LF6B84F>
2022-01-16 20:21:25 +0100

    Primer canvi (Penjar)

--- src/practica6_UF2/Ex3.java
+++ src/practica6_UF2/Ex3.java
@@ -7,5 +7,20 @@ package practica6_UF2;
  * @author Ramon
  */
 public class Ex3 {
-    
+    public static int Maxim(){
+        int[] Coleccio_numeros=new int[]{234, 666,89,11,23,8,1,3444,61};
+        //Agafem el primer com exemple
+        int major = Coleccio_numeros[0];
+        //Compara cada valor amb el primer que hem agafat, si és més gran que l'anterior es canvien de posició 
+        for(int x = 1; x<Coleccio_numeros.length; x++){
+            if (Coleccio_numeros[x]>major){
+                major = Coleccio_numeros[x];
+            }
+        }
+        return major;
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(Maxim());
+    }
 }
