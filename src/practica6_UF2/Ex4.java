e1a64373a751041f5458688dcf12e5912bf485d3
Ramon <Ramon@DESKTOP-LF6B84F>
2022-01-16 14:22:11 +0100

    1r canvi (penjar)

---
+++ src/practica6_UF2/Ex4.java
@@ -0,0 +1,22 @@
+/*4. Escriu un procediment anomenat “Dau” que escrigui per pantalla un enter a
+l’atzar que estigui comprès entre 1 i 6.*/
+package practica6_UF2;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Ramon
+ */
+public class Ex4 {
+
+    public static int Dau() {
+        int numero;
+        Random numero_aleatori = new Random();
+        numero = numero_aleatori.nextInt(6) + 1;
+        return numero;
+    }
+    public static void main(String[] args) {
+        System.out.println("Tires un dau i surt... " + Dau());
+    }
+}
