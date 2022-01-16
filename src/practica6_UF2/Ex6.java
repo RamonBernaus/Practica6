c053bdcf0327ce7930d552f57c54f5585009335c
Ramon <LINKAT@DESKTOP-LF6B84F>
2022-01-16 17:35:53 +0100

    1r canvi (penjar)

--- src/practica6_UF2/Ex6.java
+++ src/practica6_UF2/Ex6.java
@@ -7,5 +7,24 @@ package practica6_UF2;
  * @author Ramon
  */
 public class Ex6 {
-    
+
+    public static String Inicials() {
+        int ContadorEspaisBlanc = 0;
+        String frase = "hola em dic josep";
+        String result = frase;
+        char[] may = frase.toCharArray();
+        for (int i = 0; i < may.length; i++) {
+            if (frase.charAt(i) == ' ') {
+                ContadorEspaisBlanc++;
+                may[0] = Character.toUpperCase(may[0]);
+                result = may[0] + result;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Inicials());
+    }
 }
