7449ee3c466fb4f6e911e3df0ab0c92ac2324e18
Ramon <Ramon@DESKTOP-LF6B84F>
2022-01-16 15:46:24 +0100

    1r canvi (penjar)

---
+++ src/practica6_UF2/Ex5.java
@@ -0,0 +1,12 @@
+/*5. Escriu una funció “MCD_Euclides” que retorni el màxim comú divisor de dos
+números segons l’algorisme d’Euclides. Aquest algorisme el podeu trobar
+detallat a https://ca.wikipedia.org/wiki/Algorisme_d%27Euclides */
+package practica6_UF2;
+
+/**
+ *
+ * @author Ramon
+ */
+public class Ex5 {
+    
+}
