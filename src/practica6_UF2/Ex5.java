c1e4cf88040d21f98a04c4fd68fd33b2c1daec80
Eric <LINKAT@DESKTOP-LF6B84F>
2022-01-16 20:29:04 +0100

    Primer canvi (Penjar)

--- src/practica6_UF2/Ex5.java
+++ src/practica6_UF2/Ex5.java
@@ -1,12 +1,36 @@
-/*5. Escriu una funció “MCD_Euclides” que retorni el màxim comú divisor de dos
+/*
+5. Escriu una funció “MCD_Euclides” que retorni el màxim comú divisor de dos
 números segons l’algorisme d’Euclides. Aquest algorisme el podeu trobar
-detallat a https://ca.wikipedia.org/wiki/Algorisme_d%27Euclides */
+detallat a https://ca.wikipedia.org/wiki/Algorisme_d%27Euclides
+ */
 package practica6_UF2;
+
+
 
 /**
  *
- * @author Ramon
+ * @author Eric
  */
 public class Ex5 {
-    
+    // Algoritmo de Euclides
+
+    public static int MCD_Euclides() {
+
+        int a, b;
+
+        a = 4;
+        b = 6;
+        while (a != b) {
+            if (a < b) {
+                b = b - a;
+            } else {
+                a = a - b;
+            }
+        }
+        return (a);
+    }
+
+    public static void main(String[] args) {
+  System.out.println(MCD_Euclides());
+    }
 }
