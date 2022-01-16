a9bc45b4a7fc3cdc9f4260c3c03a3771b4abc908
Ramon <Ramon@DESKTOP-LF6B84F>
2022-01-16 15:46:05 +0100

    1r canvi (penjar)

---
+++ src/practica6_UF2/Ex3.java
@@ -0,0 +1,11 @@
+/*3. Escriu una funció anomenada “Maxim” al que se li passa un vector d’enters i
+ha de tornar el més gran dels números que conté el vector*/
+package practica6_UF2;
+
+/**
+ *
+ * @author Ramon
+ */
+public class Ex3 {
+    
+}
