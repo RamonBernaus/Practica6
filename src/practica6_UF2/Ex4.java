7b0fcb0d0ac2a46a98f276acd2152d33e3c155b7
LINKAT <LINKAT@DESKTOP-LF6B84F>
2022-01-16 21:46:43 +0100

    Comentar

--- src/practica6_UF2/Ex4.java
+++ src/practica6_UF2/Ex4.java
@@ -12,12 +12,12 @@ public class Ex4 {
 
     public static int Dau() {
         int numero;
-        Random numero_aleatori = new Random();
-        numero = numero_aleatori.nextInt(6) + 1;
-        return numero;
+        Random numero_aleatori = new Random(); //Creem un numero 'random' (aleatori) 
+        numero = numero_aleatori.nextInt(6) + 1; //Fem que el int es torni aleatori entre 6 de maxim i 1 de minim 
+        return numero; //retornem aquest numero 
     }
 
     public static void main(String[] args) {
-        System.out.println("Tires un dau i surt... " + Dau());
+        System.out.println("Tires un dau i surt... " + Dau()); //Imprimim el programa anterior
     }
 }
